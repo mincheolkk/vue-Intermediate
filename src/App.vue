c082fbaec64cba31c9b40a55be5042366228154f
mincheolkk <kmc04080408@gmail.com>
2022-06-22 21:47:50 +0900

    todoList의 data, created 를 App 으로 옮기고 props를 이용해 데이터 내려줌

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoList></TodoList>
     <TodoInput></TodoInput>
+    <TodoList v-bind:propsdata="todoItems"></TodoList>
     <TodoFooter></TodoFooter>
   </div>
 </template>
@@ -17,7 +17,21 @@ import TodoFooter from './components/TodoFooter.vue'
 
 
 export default {
-  name: 'App',
+  // name: "App",
+  data: function() {
+      return {
+          todoItems: []
+      }
+  },
+  created: function() {
+        if (localStorage.length > 0) {
+            for (var i =0; i<localStorage.length; i++) {
+                this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+
+            }
+        }
+  },
+
   components: {
     //컴포넌트 태그명 : 컴포넌트 내용
     'TodoHeader': TodoHeader,
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <ul>
-            <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+            <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
                 <i class="fa-solid fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" 
                     @click="toggleComplete(todoItem, index)"></i>                
                 <span v-bind:class="{textCompleted: todoItem.completed}">{{todoItem.item}}</span>
@@ -14,22 +14,7 @@
 </template>
 <script>
 export default {
-
-    data: function() {
-        return {
-            todoItems: []
-        }
-    },
-
-    created: function() {
-        if (localStorage.length > 0) {
-            for (var i =0; i<localStorage.length; i++) {
-                this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-
-                // this.todoItems.push(localStorage.key(i));
-            }
-        }
-    },
+    props: ['propsdata'],
     methods: {
         removeTodo: function(todoItem, index) {
             console.log('removeeeee');
